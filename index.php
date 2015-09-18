b609d40293448e5c46138120660fd7de676ab1e5
Mark Bridgeman <mbridgeman@mccabe.dreamhost.com>
2015-09-18 01:06:45 -0700

    Let's restart.

--- index.php
+++ index.php
@@ -1,9 +1,57 @@
 <?php
 
-include('./army.php');
+/**
+ * Welcome to BeeSlap!
+ * @author Mark Bridgeman <mark.bridgeman@gmail.com>
+ * @link http://slap.bridgeman.eu
+**/
 
-$Army = new Hive;
+// build 15 items. 3 Qs, 5 Ws, 7 Ds.
 
-$Army->buildArmy('3','5','7');
+
+$health;
+$health['queen'] = '100';
+$health['worker'] = '75';
+$health['drone'] = '50';
+
+class Army
+{
+//  function __construct() { print "in army";}
+
+  public function buildArmy($queen,$worker,$drone) {
+    $army = array();
+
+    //Build queens
+
+    for($i=1;$i<=$queen;$i++) {
+      $army[] = $this->buildSoldier('queen',$i);
+    }
+
+    //Build workers
+    for($i=1;$i<=$worker;$i++) {
+      $army[] = $this->buildSoldier('worker',$i);
+    }
+    //Build drones
+    for($i=1;$i<=$drone;$i++) {
+      $army[] = $this->buildSoldier('drone',$i);
+    }
+
+    return $army;
+  }
+
+  private function buildSoldier($type,$unit) {
+	global $health;
+    $soldier[$type][] = $health[$type];
+    return $soldier;
+
+  }
+
+}
+
+$army = new Army();
+
+$bees = $army->buildArmy(3,5,7);
+
+//print_r($bees);
 
 ?>
