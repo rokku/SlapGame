aa9738fb7c88aaac02898435a968aa1b51eb6966
Mark Bridgeman <mark.bridgeman@gmail.com>
2015-09-20 10:55:45 +0100

    'bees'

--- src/SlapGame/Army/Bees.php
+++ src/SlapGame/Army/Bees.php
@@ -13,13 +13,6 @@ interface Soldier {
 class Bees
 {
 
-  protected $_type = array(
-                      'queen' => array('attack' => 7, 'health' => 100),
-                      'worker' => array('attack' => 12, 'health' => 75),
-                      'drone' => array('attack' => 18, 'health' => 80)
-                    );
-
-
   public function buildArmy($queen,$worker,$drone) {
 
     $army = array();
