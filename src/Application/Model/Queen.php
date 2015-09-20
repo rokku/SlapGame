9d033004fb3f62c297979df6d8f0801e78d5da3d
Mark Bridgeman <mark.bridgeman@gmail.com>
2015-09-20 10:07:20 +0100

    building in inheritance

--- src/Application/Model/Queen.php
+++ src/Application/Model/Queen.php
@@ -15,7 +15,7 @@ class Queen extends Bees implements Soldier
   public function create() {
 
     $army = array();
-echo $this->num_of_soldiers;
+echo $num_of_soldiers;
     for($i=0;$i>=$this->num_of_soldiers;$i++) {
 
         $army[] = $this->buildSolider($this->health,$this->rank,$this->attack);
